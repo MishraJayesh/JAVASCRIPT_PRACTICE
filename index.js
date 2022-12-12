3a6b03e75e2efa73c80403bf896573e09e469e8c
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-12-12 09:51:16 +0530

    Adding Practice Set 1 Of JS

---
+++ index.js
@@ -0,0 +1,30 @@
+// Q1
+let a = "Jayesh";
+let b = 02;
+console.log(a + b);
+
+// Q2
+console.log(typeof (a + b));
+
+// Q3
+const c = {
+    name: "Jayesh",
+    age: 22,
+    map: false
+}
+// c = "hobbies"
+// c = 23
+
+// Q4
+c['hobby'] = "Reading"
+console.log(c)
+
+// Q5
+const dict = {
+    create : "to cause something new to happen or exist.",
+    change : "to become different or to make somebody/something different.",
+    write : "to make words, letters, etc., especially on paper using a pen or pencil.",
+    problem : "a thing that is difficult to deal with or to understand.",
+    hold : "to keep something in a certain position.",
+}
+console.log(dict.create);
