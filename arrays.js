dbff6b3ce4e42ab1c1f4afc9d05cad488975ef31
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-12-13 11:11:33 +0530

    Adding String | String PS | Array

---
+++ arrays.js
@@ -0,0 +1,13 @@
+let marks = [91, 82, 63, 85, false, "Not Present"];
+console.log(marks[0]);
+console.log(marks[1]);
+console.log(marks[2]);
+console.log(marks[3]);
+console.log(marks[4]);
+console.log(marks[5]);
+console.log(marks[6]);
+console.log("The length of marks is", marks.length);
+marks[6] = 89;
+marks[0] = 98;
+console.log("The length of marks is", marks.length);
+console.log(marks);
