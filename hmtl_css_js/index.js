eb401bc1b5d4e2fe7d9fcbaf7fee7cbfa3d07e42
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-12-18 21:47:42 +0530

    Adding  JavaScript In Browser And DOM BOM

---
+++ hmtl_css_js/index.js
@@ -0,0 +1,44 @@
+// JavaScript In Browser
+
+alert("Your JavaScript Is Running"); // Gives an alert.
+console.log("Hey This Is Console"); // Print the following sentence in console.
+//
+console.log(console); // Print the console object in console.
+//
+console.error("Hey This Is An Error"); // Print an error in the console.
+//
+console.assert(2 > 16); // Assert a condition.
+console.assert(20 > 16); // Assert a condition.
+console.log(console.assert); // Shows that console.assert is a function.
+//
+obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
+console.log(obj); // Print the object.
+console.table(obj); // Print the table of an object.
+//
+console.warn("Hey This Is A Warning"); // Print a warning in a console.
+//
+console.info("Hey This Is An Important Information"); // Print an information in a console.
+//
+console.time("How Many Seconds Did It Taken?"); // Print time taken to run any code in JavaScript.
+console.timeEnd("How Many Seconds Did It Taken?");
+//
+alert(`This Is An \n "ALERT" `);
+//
+confirm("Do You Want To Print Your Name?");
+//
+let a = prompt("Enter Your Name : ");
+document.write(`Your Name Is : ${a}, \n Hey ${a} How Are You Doing?`);
+console.log(`"${a}"`);
+
+
+
+// DOM_BOM
+console.log(window);
+//
+// window.console.log(window);
+console.log(document);
+//
+console.log(document.body); // Print Body Of Html
+//
+document.body.style.background = "Khaki";
+
---
+++ html_css_js_2/index.js
@@ -0,0 +1,49 @@
+let list = document.getElementById("list");
+let list2 = document.getElementById("list2");
+console.log(list.firstElementChild.textContent);
+console.log(list.firstElementChild);
+console.log(list2.firstElementChild);
+console.log(list2.firstElementChild.textContent);
+console.log(list2.lastElementChild.textContent);
+console.log(list2.lastElementChild);
+console.log(list.lastElementChild);
+console.log(list.lastElementChild.textContent);
+//
+//
+console.log(list.firstChild);
+console.log(list2.firstChild);
+console.log(list2.lastChild);
+console.log(list.lastChild);
+//
+//
+document.getElementById("list");
+list.style.backgroundColor = "red";
+//
+//
+document.getElementById("list2");
+list2.style.backgroundColor = "yellow";
+list2.style.borderRadius = "20px";
+//
+//
+let list3 = document.getElementById("list3");
+list3.style.backgroundColor = "green";
+//
+//
+document.getElementsByClassName("list");
+list.style.border = "4px solid white";
+//
+//
+document.getElementsByClassName("list");
+list2.style.border = "4px solid black";
+//
+//
+document.getElementsByClassName("list");
+list3.style.border = "4px solid pink";
+//
+//
+document.getElementsByTagName("span");
+span.style.color = "red";
+//
+//
+document.getElementsByTagName("span1");
+span1.style.color = "red";
