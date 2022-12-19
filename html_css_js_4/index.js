69f6464ea7d6adad1b7258295957f1f833f098da
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-12-19 20:27:18 +0530

    Adding matches || closest || contains

--- html_css_js_4/index.js
+++ html_css_js_4/index.js
@@ -4,10 +4,33 @@
 // ctitle.style.color = "red";
 //
 //
-let ctitles = document.querySelectorAll(".card-title");
-ctitles[0].style.color = "green";
-ctitles[1].style.color = "red";
-ctitles[2].style.color = "blue";
+// let ctitles = document.querySelectorAll(".card-title");
+// ctitles[0].style.color = "green";
+// ctitles[1].style.color = "red";
+// ctitles[2].style.color = "blue";
 //
 //
-console.log(document.getElementsByTagName("a"));
+// console.log(document.getElementsByTagName("a"));
+//
+//
+// let id = document.getElementById("btn");
+// console.log(id);
+// console.log(id.matches(".card-title"));
+// console.log(id.matches(".btn"));
+//
+//
+// let id1 = document.getElementById("thirdcardtitle");
+// console.log(id1);
+// console.log(id1.matches(".card-title"));
+// console.log(id1.matches(".btn"));
+//
+//
+// console.log(btn.closest(".card-body"));
+// console.log(thirdcardtitle.closest("#thirdcardtitle"));
+//
+//
+let id = document.getElementById("cardcont");
+console.log(id.contains(id));
+console.log(id.contains(crdimg));
+console.log(id.contains(firstcardtitle));
+
