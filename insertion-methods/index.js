e7c8c1c116a30d099293a6d64d4d3400f021d999
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-12-21 21:06:58 +0530

    Adding Event And DOM Properties

---
+++ insertion-methods/index.js
@@ -0,0 +1,83 @@
+// let a = document.getElementsByTagName('div')[0];
+// a.innerHTML = a.innerHTML + '<h1>Hello World!</h1>';
+
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+// let div = document.createElement('div');
+// div.innerHTML = '<h1>Hello World!</h1>';
+// a.append(div);
+// a.prepend(div);
+// a.before(div);
+// a.after(div);
+// a.replaceWith(div);
+
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+// let a = document.getElementById("first");
+// a.insertAdjacentHTML("beforebegin", "<br/><div>beforebegin</div>");
+// a.insertAdjacentHTML("beforeend", "<br/><div>beforeend</div>");
+// a.insertAdjacentHTML("afterbegin", "<br/><div>afterbegin</div>");
+// a.insertAdjacentHTML("afterend", "<br/><div>afterend</div>");
+// a.remove();
+
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+//  className And classList
+
+// let a = document.getElementById("first");
+// a.className = "red text-dark";
+// console.log(a.classList);
+// a.classList.add("red");
+// a.classList.add("text-dark");
+// a.classList.remove("red");
+// a.classList.remove("text-dark");
+// console.log(a.classList.toggle("red"));
+// console.log(a.classList.toggle("text-dark"));
+// console.log(a.classList.contains("red"));
+// console.log(a.classList.contains("text-dark"));
+
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+// console.log(a.classList.add("red"));
+// console.log(a.classList.add("text-dark"));
+// console.log(a.classList.remove("red"));
+// console.log(a.classList.remove("text-dark"));
+
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+//  setTimeout And setInterval
+
+// document.write("Hello World!");
+
+// let a = setTimeout(function () {
+//     alert("setTimeout Is Running");
+// }, 2000)
+
+// let b = prompt(" Do You Want To Run setTimeout ? ");
+// if ("N" == b) {
+//     clearTimeout(a);
+// }
+// console.log(a);
+
+// const sum = (a, b, c) => {
+//     console.log("Yes I Am Running " + (a + b + c));
+// }
+// setTimeout(sum, 2000, 1, 2, 3);
+
+// setInterval(function () {
+//     alert("SetTime Interval Is Running");
+// },2000)
+
+// const sum = (a, b, c) => {
+//     console.log("Yes I Am Running " + (a + b + c));
+// }
+// setInterval(sum, 2000, 1, 2, 3);
+
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
