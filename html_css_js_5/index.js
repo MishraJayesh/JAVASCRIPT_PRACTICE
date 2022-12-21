405ee9341d4437f47fe8d51a6b46bee266ad1443
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-12-21 13:07:16 +0530

    Adding Inner HTML || Outer HTML || Attribute Method

---
+++ html_css_js_5/index.js
@@ -0,0 +1,39 @@
+// let a = document.getElementsByTagName('span')[0];
+// console.log(a);
+// console.dir(a);
+//
+// console.log(document.getElementsByTagName('span')[0]);
+// console.dir(document.getElementsByTagName('span')[0]);
+//
+// console.log(document.body.firstChild.nodeName);
+// console.log(document.body.firstChild.tagName);
+// console.log(document.body.firstElementChild.nodeName);
+// console.log(document.body.firstElementChild.tagName);
+//
+// console.log(crdtitle.innerHTML);
+// console.log(crdtxt.innerHTML);
+// console.log(crdbody.innerHTML);
+// console.log(span.innerHTML);
+//
+// console.log(crdtitle.outerHTML);
+// console.log(crdtxt.outerHTML);
+// console.log(crdbody.outerHTML);
+// console.log(span.outerHTML);
+//
+// console.log(crdtitle.textContent);
+// console.log(crdtxt.textContent);
+// console.log(crdbody.textContent);
+// console.log(span.textContent);
+//
+// console.log(span.hidden = false);
+//
+let a = first.getAttribute("className");
+let b = first.hasAttribute("className");
+// let c = first.setAttribute("hidden","true");
+console.log(a);
+console.log(b);
+// console.log(c);
+console.log(first.removeAttribute("className"));
+//
+console.log(first.attributes);
+
